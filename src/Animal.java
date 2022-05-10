bb08c6e9a53354a5cdcf8c2694e4a17b7fcdb740
shaikhsirin <shaikhsirin8080example.com>
2022-05-10 20:57:20 +0530

    Added:-stores actual and guess number

---
+++ src/Animal.java
@@ -0,0 +1,24 @@
+public class Animal {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Dog extends AnimalT {
+    void bark() {
+        System.out.println("barking");
+
+    }
+}
+
+class AnimalT {
+
+    public void eat() {
+        System.out.println("eating...");
+    }
+
+
+}
+
+
+
---
+++ src/GuessNumber.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+
+        // stores actual and guess number
+        int answer, guess;
+
+    }
+}
