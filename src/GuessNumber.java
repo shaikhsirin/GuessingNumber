e3e5b1f4737a2637bd035bc26c6eaae90e5a2e1a
shaikhsirin <shaikhsirin8080example.com>
2022-05-10 21:17:35 +0530

    Added:- loop until guess correct

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -12,4 +12,43 @@ public class GuessNumber {
         final int min = 0;
         int mid = (min + MAX) / 2;
 
+        // takes input using scanner
+        Scanner in = new Scanner(System.in);
 
+        // Random instance
+        Random rand = new Random();
+
+        boolean correct = false;
+
+        // correct answer
+        answer = rand.nextInt(MAX) + 1;
+
+        // loop until the guess is correct
+        while (!correct) {
+
+            System.out.println("Guess a number between 1 and 100: ");
+
+            // guess value
+            guess = in.nextInt();
+
+            // if guess is greater than actual
+            if (guess > answer) {
+                System.out.println("Too high, try again");
+            }
+
+            // if guess is less than actual
+            else if (guess < answer) {
+                System.out.println("Too low, try again");
+            }
+
+            // guess is equal to actual value
+            else {
+
+                System.out.println("Yes, you guessed the number.");
+
+                correct = true;
+            }
+        }
+        System.exit(0);
+    }
+}
